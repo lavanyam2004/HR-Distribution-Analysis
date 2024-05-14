27f0cbb0c75f21505faa489b3969d14cce7926e5
lavanyam2004 <lavanyamuthia@gmail.com>
2024-05-14 22:42:02 +0800

    Add files via upload

---
+++ ExploratoryHRDataAnalytics.sql
@@ -0,0 +1,158 @@
+#line2-63 is cleaning of the dataset
+CREATE DATABASE projects;
+
+USE projetcs;
+
+SELECT * FROM hr;
+
+ALTER TABLE hr
+CHANGE COLUMN ï»¿id emp_id VARCHAR(20) NULL;
+
+DESCRIBE hr;
+
+SELECT birthdate FROM hr;
+
+SET sql_safe_updates = 0;
+
+UPDATE hr
+SET birthdate = CASE
+	WHEN birthdate LIKE '%/%' THEN date_format(str_to_date(birthdate, '%m/%d/%Y'), '%Y-%m-%d')
+    WHEN birthdate LIKE '%-%' THEN date_format(str_to_date(birthdate, '%m-%d-%Y'), '%Y-%m-%d')
+    ELSE NULL
+END;
+
+ALTER TABLE hr
+MODIFY COLUMN birthdate DATE;
+
+UPDATE hr
+SET hire_date = CASE
+	WHEN hire_date LIKE '%/%' THEN date_format(str_to_date(hire_date, '%m/%d/%Y'), '%Y-%m-%d')
+    WHEN hire_date LIKE '%-%' THEN date_format(str_to_date(hire_date, '%m-%d-%Y'), '%Y-%m-%d')
+    ELSE NULL
+END;
+
+ALTER TABLE hr
+MODIFY COLUMN hire_date DATE;
+
+UPDATE hr
+SET termdate = date(str_to_date(termdate, '%Y-%m-%d %H:%i:%s UTC'))
+WHERE termdate IS NOT NULL AND termdate != ' ';
+
+ALTER TABLE hr
+MODIFY COLUMN termdate DATE;
+
+ALTER TABLE hr ADD COLUMN age INT;
+
+UPDATE hr
+SET age = timestampdiff(YEAR, birthdate, CURDATE());
+
+SELECT 
+	min(age) AS youngest,
+    max(age) AS oldest
+FROM hr;
+
+SELECT count(*) FROM hr WHERE age < 18;
+
+SELECT COUNT(*) FROM hr WHERE termdate > CURDATE();
+
+SELECT COUNT(*)
+FROM hr
+WHERE termdate = '0000-00-00';
+
+SELECT location FROM hr;
+
+#race breakdown of employees
+SELECT race, COUNT(*) AS count
+FROM hr
+WHERE age >= 18
+GROUP BY race
+ORDER BY count DESC
+
+#location distribution of employees by state 
+SELECT location_state, COUNT(*) as count
+FROM hr
+WHERE age >= 18
+GROUP BY location_state
+ORDER BY count DESC;
+
+#gender breakdown of employees 
+SELECT gender, COUNT(*) AS count
+FROM hr
+WHERE age >= 18
+GROUP BY gender
+
+#gender distribution variation across departments 
+SELECT department, gender, COUNT(*) as count
+FROM hr
+WHERE age >= 18
+GROUP BY department, gender
+ORDER BY department;
+
+#age distribution of employees 
+SELECT 
+  MIN(age) AS youngest,
+  MAX(age) AS oldest
+FROM hr
+WHERE age >= 18
+
+SELECT FLOOR(age/10)*10 AS age_group, COUNT(*) AS count
+FROM hr
+WHERE age >= 18
+GROUP BY FLOOR(age/10)*10;
+
+SELECT 
+  CASE 
+    WHEN age >= 18 AND age <= 24 THEN '18-24'
+    WHEN age >= 25 AND age <= 34 THEN '25-34'
+    WHEN age >= 35 AND age <= 44 THEN '35-44'
+    WHEN age >= 45 AND age <= 54 THEN '45-54'
+    WHEN age >= 55 AND age <= 64 THEN '55-64'
+    ELSE '65+' 
+  END AS age_group, 
+  COUNT(*) AS count
+FROM 
+  hr
+WHERE 
+  age >= 18
+GROUP BY age_group
+ORDER BY age_group;
+
+#gender and age distribution of employees 
+SELECT 
+  CASE 
+    WHEN age >= 18 AND age <= 24 THEN '18-24'
+    WHEN age >= 25 AND age <= 34 THEN '25-34'
+    WHEN age >= 35 AND age <= 44 THEN '35-44'
+    WHEN age >= 45 AND age <= 54 THEN '45-54'
+    WHEN age >= 55 AND age <= 64 THEN '55-64'
+    ELSE '65+' 
+  END AS age_group, gender,
+  COUNT(*) AS count
+FROM 
+  hr
+WHERE 
+  age >= 18
+GROUP BY age_group, gender
+ORDER BY age_group, gender;
+
+#distribution of job title of employees 
+SELECT jobtitle, COUNT(*) as count
+FROM hr
+WHERE age >= 18
+GROUP BY jobtitle
+ORDER BY jobtitle DESC;
+
+#employee count how it has changed over time (table of year, hired, terminated, net change, net change percentage)
+SELECT 
+    YEAR(hire_date) AS year, 
+    COUNT(*) AS hires, 
+    SUM(CASE WHEN termdate <> '0000-00-00' AND termdate <= CURDATE() THEN 1 ELSE 0 END) AS terminations, 
+    COUNT(*) - SUM(CASE WHEN termdate <> '0000-00-00' AND termdate <= CURDATE() THEN 1 ELSE 0 END) AS net_change,
+    ROUND(((COUNT(*) - SUM(CASE WHEN termdate <> '0000-00-00' AND termdate <= CURDATE() THEN 1 ELSE 0 END)) / COUNT(*) * 100),2) AS net_change_percent
+FROM 
+    hr
+WHERE age >= 18
+GROUP BY 
+    YEAR(hire_date)
+ORDER BY 
+    YEAR(hire_date) ASC;
